6c1e3223c7f89ec6ec5efa21db5fe5abe8c08d57
chan pagne <chnpgn@gmail.com>
2025-12-29 02:54:13 +0300

    Managed to create ALL the list pages for ALL thecomponents

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -16,7 +16,7 @@ const Table = ({ columns, data, renderRow }: Props) => {
       <thead>
         <tr className="text-left text-gray-500 text-sm">
           {columns.map((col) => (
-            <th key={col.accessor}>{col.header}</th>
+            <th key={col.accessor} className={col.className}>{col.header}</th>
           ))}
         </tr>
       </thead>
